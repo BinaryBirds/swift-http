0ca75b0e00e2605271e0d48acd3d2b4a90168863
Tibor Bodecs <mail.tib@gmail.com>
2022-03-10 14:55:03 +0100

    download & upload requests

--- Sources/SwiftHttp/HttpClient.swift
+++ Sources/SwiftHttp/HttpClient.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public protocol HttpClient {
-    func request(_ req: HttpRequest) async throws -> HttpResponse
 
-//    func download(_ req: HttpRequest) async throws -> HttpResponse
-//    func upload(_ req: HttpRequest) async throws -> HttpResponse
+    func request(_ req: HttpRequest) async throws -> HttpResponse
+    func download(_ req: HttpRequest) async throws -> HttpResponse
+    func upload(_ req: HttpRequest) async throws -> HttpResponse
 }
 
 
--- Tests/SwiftHttpTests/Helpers/UrlSessionHttpClient.swift
+++ Tests/SwiftHttpTests/Helpers/UrlSessionHttpClient.swift
@@ -10,15 +10,65 @@ import SwiftHttp
 
 struct UrlSessionHttpClient: HttpClient {
     
-    var log: Bool
+    let session: URLSession
+    let log: Bool
+    
+    init(session: URLSession = .shared, log: Bool = false) {
+        self.session = session
+        self.log = log
+    }
 
     func request(_ req: HttpRequest) async throws -> HttpResponse {
         let urlRequest = req.urlRequest
         if log {
             print(urlRequest.curlString)
         }
-        let res = try await URLSession.shared.data(for: urlRequest, delegate: nil)
+        let res = try await session.data(for: urlRequest)
+        return try HttpDataResponse(res)
+    }
+    
+    func upload(_ req: HttpRequest) async throws -> HttpResponse {
+        let urlRequest = req.urlRequest
+        if log {
+            print(urlRequest.curlString)
+        }
+        
+        let res: (Data, URLResponse) = try await withUnsafeThrowingContinuation { c in
+            session.uploadTask(with: urlRequest, from: urlRequest.httpBody) { data, response, error in
+                if let error = error {
+                    return c.resume(throwing: error)
+                }
+                guard let data = data, let urlResponse = response else {
+                    return c.resume(throwing: HttpError.invalidResponse)
+                }
+                c.resume(returning: (data, urlResponse))
+            }.resume()
+        }
+        return try HttpDataResponse(res)
+    }
+    
+    /// returns the path data of the URL
+    func download(_ req: HttpRequest) async throws -> HttpResponse {
+        let urlRequest = req.urlRequest
+        if log {
+            print(urlRequest.curlString)
+        }
+
+        let res: (Data, URLResponse) = try await withUnsafeThrowingContinuation { c in
+            session.downloadTask(with: urlRequest) { url, response, error in
+                if let error = error {
+                    return c.resume(throwing: error)
+                }
+                guard let urlResponse = response, let url = url, let pathData = url.path.data(using: .utf8) else {
+                    return c.resume(throwing: HttpError.invalidResponse)
+                }
+                c.resume(returning: (pathData, urlResponse))
+            }
+            .resume()
+        }
         return try HttpDataResponse(res)
     }
 }
+
+
 
