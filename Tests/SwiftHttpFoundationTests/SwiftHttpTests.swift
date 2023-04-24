dd9182d16ac410c6a4656d9187a9dc79f99affe2
Tibor Bodecs <mail.tib@gmail.com>
2023-04-24 21:54:31 +0200

    enabled other tests

--- Tests/SwiftHttpFoundationTests/SwiftHttpTests.swift
+++ Tests/SwiftHttpFoundationTests/SwiftHttpTests.swift
@@ -1,46 +1,45 @@
 import XCTest
-
 @testable import SwiftHttp
 
-//final class SwiftHttpTests: XCTestCase {
-//
-//    func testCancellation() async throws {
-//        let task = Task {
-//            let api = PostApi()
-//            _ = try await api.listPosts()
-//            XCTFail("Request should be cancelled")
-//        }
-//
-//        let deadline = DispatchTime.now() + .milliseconds(10)
-//        DispatchQueue.global().asyncAfter(deadline: deadline) {
-//            task.cancel()
-//        }
-//
-//        do {
-//            let _ = try await task.value
-//        }
-//        catch {
-//            XCTAssertTrue(error is URLError)
-//            XCTAssertEqual((error as? URLError)?.code, .cancelled)
-//        }
-//    }
-//
-//    func testError() async throws {
-//        let api = PostApi()
-//        do {
-//            _ = try await api.invalidApiCall()
-//        }
-//        catch HttpError.invalidStatusCode(let res) {
-//            XCTAssertEqual(res.statusCode, .notFound)
-//        }
-//        catch {
-//            XCTFail(error.localizedDescription)
-//        }
-//    }
-//
-//    func testQueryParams() async throws {
-//        let api = PostApi()
-//        let res = try await api.filterPosts(1)
-//        XCTAssertEqual(res.count, 10)
-//    }
-//}
+final class SwiftHttpTests: XCTestCase {
+
+    func testCancellation() async throws {
+        let task = Task {
+            let api = PostApi()
+            _ = try await api.listPosts()
+            XCTFail("Request should be cancelled")
+        }
+
+        let deadline = DispatchTime.now() + .milliseconds(10)
+        DispatchQueue.global().asyncAfter(deadline: deadline) {
+            task.cancel()
+        }
+
+        do {
+            let _ = try await task.value
+        }
+        catch {
+            XCTAssertTrue(error is URLError)
+            XCTAssertEqual((error as? URLError)?.code, .cancelled)
+        }
+    }
+
+    func testError() async throws {
+        let api = PostApi()
+        do {
+            _ = try await api.invalidApiCall()
+        }
+        catch HttpError.invalidStatusCode(let res) {
+            XCTAssertEqual(res.statusCode, .notFound)
+        }
+        catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testQueryParams() async throws {
+        let api = PostApi()
+        let res = try await api.filterPosts(1)
+        XCTAssertEqual(res.count, 10)
+    }
+}
